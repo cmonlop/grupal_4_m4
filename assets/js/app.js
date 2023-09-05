44fd117f5b1f8b10fe1aae7e806a15e9542da069
cmonlop <cmontlop@gmail.com>
2023-09-05 15:55:12 -0300

    primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+//2. Crear una función asíncrona para obtener los datos de la URL (Ceci con Karla).
+// Dentro de un bloque try/catch, utilizar el método fetch mediante la instrucción
+// await para recibir el valor directamente de la promesa. (3 punto)
+
+// 5. Crear una función que retorne una promesa después de 2 segundos utilizando
+// setTimeout. El mensaje a retornar en el bloque try debe ser un mensaje que
+// indique: “Información Enviada”. (2 puntos)
+
+
+
+
+
+
+
+//3. Genera la conversión de grados Kelvin a Celsius (Zimram, Andrea Gonzalo)
+
+
+
+
+
+//
+
+
